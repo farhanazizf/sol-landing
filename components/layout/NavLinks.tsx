b401b078e2b1cb680acce139e064fabb56795763
farhanazizf <farhanazizf7@gmail.com>
2025-01-15 16:46:14 +0700

    chore: configure nav

--- components/layout/NavLinks.tsx
+++ components/layout/NavLinks.tsx
@@ -5,6 +5,8 @@ import { Button } from "@/components/ui/button";
 // import { NavigationLinks } from "./MobileMenu";
 import { trackNavigation } from "@/lib/analytics";
 import { CartButton } from "../shop/navigation/CartButton";
+import { useCart } from "@/hooks/useCart";
+import { useEffect, useState } from "react";
 // import { useCart } from "@/hooks/useCart";
 
 const links = [
@@ -16,8 +18,15 @@ const links = [
 ];
 
 export const NavLinks = ({ onLinkClick }: { onLinkClick?: () => void }) => {
-  // const { cart } = useCart();
-  // const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
+  const [showNav, setShowNav] = useState(true);
+
+  useEffect(() => {
+    // Check if user has already registered
+    const userId = localStorage.getItem("shopUserId");
+    if (userId) {
+      setShowNav(false);
+    }
+  }, []);
 
   const handleClick = (label: string) => {
     trackNavigation(label);
@@ -26,27 +35,28 @@ export const NavLinks = ({ onLinkClick }: { onLinkClick?: () => void }) => {
 
   return (
     <div className="hidden md:flex items-center gap-8">
-      {links.map((link) => (
+      {!showNav ? (
         <Link
-          key={link.href}
-          href={link.href}
+          key={"/shop"}
+          href={"/shop"}
           className="text-[#666] hover:text-[#444] transition-colors text-sm font-medium"
-          onClick={() => handleClick(link.label)}
+          onClick={() => handleClick("Shop")}
         >
-          {link.label}
+          {"Shop"}
         </Link>
-      ))}
-      {/* {links.map((link) => (
-        <Link
-          key={link.href}
-          href={link.href}
-          className="text-[#666] hover:text-[#444] transition-colors text-sm font-medium"
-          onClick={() => handleClick(link.label)}
-        >
-          {link.label}
-        </Link>
-      ))} */}
-      {/* <NavigationLinks className="flex items-center gap-8" /> */}
+      ) : (
+        links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className="text-[#666] hover:text-[#444] transition-colors text-sm font-medium"
+            onClick={() => handleClick(link.label)}
+          >
+            {link.label}
+          </Link>
+        ))
+      )}
+
       <CartButton />
       <Button
         className="bg-[#4052B5] hover:bg-[#3445A3] text-sm font-medium px-6"
