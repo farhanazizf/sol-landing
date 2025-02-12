1a2fc790219367fd9dd40bfacc57e99dc49b840d
farhanazizf <farhanazizf7@gmail.com>
2025-02-12 11:42:35 +0700

    chore: update favicon assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,13 +13,53 @@ export const metadata: Metadata = {
   title: "Sukanda OneLink",
   description:
     "Platform B2B Online untuk memudahkan Anda mengakses semua informasi produk dan pemesanan produk dimanapun, kapanpun.",
-  icons: {
-    icon: [
-      {
-        url: "/images/logo/sol-logo-big.png",
-      },
-    ],
-  },
+  icons: [
+    {
+      url: "/images/favicon/sol-icon-36x36.png",
+      sizes: "36x36",
+      type: "image/png",
+    },
+    {
+      url: "/images/favicon/sol-icon-48x48.png",
+      sizes: "48x48",
+      type: "image/png",
+    },
+    {
+      url: "/images/favicon/sol-icon-72x72.png",
+      sizes: "72x72",
+      type: "image/png",
+    },
+    {
+      url: "/images/favicon/sol-icon-96x96.png",
+      sizes: "96x96",
+      type: "image/png",
+    },
+    {
+      url: "/images/favicon/sol-icon-144x144.png",
+      sizes: "144x144",
+      type: "image/png",
+    },
+    {
+      url: "/images/favicon/apple-touch-icon.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+    {
+      url: "/images/favicon/sol-icon-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      url: "/images/favicon/sol-icon-256x256.png",
+      sizes: "256x256",
+      type: "image/png",
+    },
+    {
+      url: "/images/favicon/sol-icon-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+    },
+  ],
 };
 
 export default function RootLayout({
